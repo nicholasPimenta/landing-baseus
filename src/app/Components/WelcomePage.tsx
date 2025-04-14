f5892e5d05b251d51f96801451619252ace0b44a
Nicholas Pimenta <nicholas2ilva@gmail.com>
2025-04-14 13:54:44 -0300

    Margin Adjustment in the Initial Part

--- src/app/Components/WelcomePage.tsx
+++ src/app/Components/WelcomePage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function WelcomePage() {
   return (
     <div id="hero" className="h-screen flex flex-col-reverse lg:flex-row justify-around items-center mb-8 bg-blue-700">
-      <div className="flex flex-col justify-start">
+      <div className="flex flex-col justify-start mb-8 lg:mb-0">
         <h1 className="font-bold lg:text-left text-center text-4xl lg:text-5xl text-neutral-100">Baseus Bowie E18</h1>
         <p className="font-light lg:text-left text-center text-lg lg:text-xl text-neutral-100">Descubra a revolução do áudio Bluetooth.</p>
       </div>
